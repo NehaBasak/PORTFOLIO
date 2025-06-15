58caecb393a5ae1cdba0f8d59fd6f74e1f67ec71
NehaBasak <nehabasak7101996@gmail.com>
2025-06-15 13:38:05 +0530

    change css, javascript ,html

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ const navRight = document.querySelector(".nav-right");
 console.log(navRight);
 const list = document.querySelector(".icon");
 console.log(list);
-list.addEventListener("click", ()=>{
-    console.log(navRight.classList);
-    navRight.classList.toggle("active");
+list.addEventListener("click", () => {
+  console.log(navRight.classList);
+  navRight.classList.toggle("active");
 });
 
 // Small Education Section
@@ -26,3 +26,33 @@ function prevSlide() {
   currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
   updateSliderPosition();
 }
+
+// h1>span
+const textElement = document.getElementById("typing-text");
+const word = "FRONTEND DEVELOPER";
+let index = 0;
+let isDeleting = false;
+function type() {
+  if (!isDeleting) {
+    textElement.textContent = word.substring(0, index + 1);
+    index++;
+    if (index === word.length) {
+      setTimeout(() => {
+        isDeleting = true;
+        type();
+      }, 1500); // Pause before deleting
+      return;
+    }
+  } else {
+    textElement.textContent = word.substring(0, index - 1);
+    index--;
+    if (index === 0) {
+      isDeleting = false;
+      setTimeout(type, 2000); // 2s delay before typing again
+      return;
+    }
+  }
+  setTimeout(type, 100); // Typing/deleting speed
+}
+type();
+
