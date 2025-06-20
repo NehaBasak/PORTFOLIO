bfa1f8b2580691fb7ba93570a5c78f201a5c31f3
NehaBasak <nehabasak7101996@gmail.com>
2025-06-20 22:53:19 +0530

    css,html,javascript change

--- js/index.js
+++ js/index.js
@@ -56,11 +56,5 @@ function type() {
 }
 type();
 
-// cursor
-   const cursor = document.getElementById("cursor");
 
-    document.addEventListener("mousemove", (e) => {
-      cursor.style.left = `${e.clientX}px`;
-      cursor.style.top = `${e.clientY}px`;
-    });
 
